228ff0766abbf05b4ffebe7cad7390c70398eb12
Eddie Jaoude <eddie@jaoudestudios.com>
2014-11-27 10:42:51 +0000

    An example unit test added & linked to Travis CI

---
+++ tests/ApplicationTest/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace ApplicationTest;
+
+/**
+ * Class ModuleTest
+ *
+ * @package EddieJaoude\Zf2Logger\Tests\Zf2LoggerTest
+ */
+class ModuleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \EddieJaoude\Zf2Logger\Module
+     */
+    private $instance;
+
+    public function setUp()
+    {
+
+    }
+
+    public function testInstance()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+namespace Application\Tests;
+$vendorDir = __DIR__ . '/../vendor';
+if (file_exists($file = $vendorDir . '/autoload.php')) {
+    require_once $file;
+} else if (file_exists($file = './vendor/autoload.php')) {
+    require_once $file;
+} else {
+    throw new \RuntimeException("Not found composer autoload");
+}
