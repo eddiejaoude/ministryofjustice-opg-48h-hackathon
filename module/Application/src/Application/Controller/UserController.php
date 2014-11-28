4da744ae76c1c5f4879a96547b2c3228155dbb2b
Eddie Jaoude <eddie@jaoudestudios.com>
2014-11-28 14:46:15 +0000

    User events (filtered to tasks)

--- module/Application/src/Application/Controller/UserController.php
+++ module/Application/src/Application/Controller/UserController.php
@@ -9,11 +9,22 @@ class UserController extends AbstractActionController
 {
     public function indexAction()
     {
+        $id = $this->getRequest()->getPost('user_id', null);
+
+        if (null != $id) {
+            $taskEvents = $this->getServiceLocator()
+                ->get('application.service.user')
+                ->getTaskEventsForEmail($id);
+        } else {
+            $taskEvents = array();
+        }
+
         return new ViewModel(
             array(
             'assignees' => $this->getServiceLocator()
                             ->get('application.service.user')
-                            ->getAssignees()
+                            ->getAssignees(),
+            'taskEvents' => $taskEvents
             )
         );
     }
--- module/Application/src/Application/Service/UserService.php
+++ module/Application/src/Application/Service/UserService.php
@@ -35,4 +35,16 @@ class UserService
 
         return $resultSet;
     }
+
+    public function getTaskEventsForEmail($id)
+    {
+        $connection = $this->adapter->getDriver()->getConnection();
+        $result = $connection->execute('SELECT * FROM events WHERE user_id = ' . $id . ' AND sourceentityclass = \'Opg\Core\Model\Entity\CaseItem\Task\Task\'');
+
+        $stmt = $result->getResource();
+
+        $resultSet = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $resultSet;
+    }
 }
