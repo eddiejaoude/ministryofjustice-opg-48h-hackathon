7046f5d2eaa5b33c9882aec6b6fd1ddd597f3c4c
Paul VM <paul.vm@fullstackwebpro.co.uk>
2014-11-28 10:34:45 +0000

    Updated routes and added new controllers

---
+++ module/Application/src/Application/Controller/HeatmapController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class HeatmapController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel(
+
+        );
+    }
+}
